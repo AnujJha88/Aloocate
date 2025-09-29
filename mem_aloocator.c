ebbb01ac49bb53a1f0f33e07ccc2691ac86637e2
AnujJha88 <anujjha8788@gmail.com>
2025-09-29 19:39:25 +0530

    some more logic fixes

--- mem_aloocator.c
+++ mem_aloocator.c
@@ -48,23 +48,41 @@ block_meta_t* find_first_free(block_meta_t **last, size_t size){
     return NULL;
 }
 
+block_meta_t* find_last_block(){
+    if(!global_base) return NULL;//what else will you do with a null global base
+
+    block_meta_t* current=global_base;
+
+    while(currrent->next){
+        current=current->next;
+    }
+    return current;
+}
+
 void* my_malloc(int size){
 
     if(size<=0) return NULL;
     block_meta_t *new_mem;
     if(!global_base){
-        //TODO when we have no base we need to ask OS for memory to then use for
-        //our purposes
+        // so this is our first allocation
+        new_mem=request_space(NULL,size);
+        if(!new_mem)return NULL;
+        global_base=new_mem;
+
+
     }
     else{
         new_mem=find_first_free(&global_base,size);
         if(new_mem){
             //TODO when we have a valid block available
             new_mem->is_free=0;// will do splitting stuff later
+
         }
+
         else{
-            //TODO when we have no block available
-            //need bigger heap
+            block_meta_t* last=find_last_block();
+            new_mem=request_space(last,size);
+            if(!new_mem) return NULL;
         }
 
     }
